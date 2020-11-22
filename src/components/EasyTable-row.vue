dad8d586b9e3db656ec2b93c6fa81590fdfbf23d
easyscripter <deniscs49@gmail.com>
2020-11-22 18:23:51 +0300

    add support flex for ie11

--- src/components/EasyTable-row.vue
+++ src/components/EasyTable-row.vue
@@ -1,11 +1,14 @@
 <template>
- <tr class="EasyTable-row" >
-   <td :class="{'username': index === 0}" v-for="(value, index) in rowsArray" :key="index">{{value}}</td>
+ <tr class="EasyTable-row">
+   <td class="IE-FlexAuto" :class="{'username': index === 0}" v-for="(value, index) in rowsArray" :key="index">
+     {{ value }}
+   </td>
    <td class="delete-row" @click="$emit('deleteRow')">X</td>
  </tr>
 </template>
 
 <script>
+'use strict'
 export default {
   name: "EasyTable-row",
   props: {
@@ -23,16 +26,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .delete-row {
-    padding: 20px 15px;
-    text-align: center;
-    cursor: pointer;
-  }
-  .username {
-    color: #0b75d2;
-    font-weight: bold;
-  }
+.delete-row {
+  padding: 20px 15px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.username {
+  color: #0b75d2;
+  font-weight: bold;
+}
+
+tr {
+  width: 100%;
+  display: -webkit-flex;
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  -webkit-box-shadow: 0 4px 8px 0 rgba(34, 60, 80, 0.2);
+  -moz-box-shadow: 0 4px 8px 0 rgba(34, 60, 80, 0.2);
+  box-shadow: 0 4px 8px 0 rgba(34, 60, 80, 0.2);
+
   td {
-    flex: 1;
+    width: 300px;
+    flex-basis: 25%;
+    text-align: center;
+
+    &:-ms-fullscreen, :root .IE-FlexAuto {
+      flex-basis: auto;
+    }
   }
+}
 </style>
--- src/components/EasyTable.vue
+++ src/components/EasyTable.vue
@@ -1,9 +1,8 @@
 <template>
-<div class="table-container">
   <table class="EasyTable">
     <thead class="EasyTable-header">
       <tr>
-        <th v-for="(name, index) in columnsNameArray" :key="index">{{name}}</th>
+        <th class="IE-FlexAuto" v-for="(name, index) in columnsNameArray" :key="index">{{ name }}</th>
         <th class="deleteColumn"></th>
       </tr>
     </thead>
@@ -11,10 +10,10 @@
         <slot>Строка</slot>
     </tbody>
   </table>
-</div>
 </template>
 
 <script>
+'use strict'
 export default {
   name: "EasyTable",
   props: {
@@ -32,47 +31,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .table-container {
-    width: 100%;
-    display: flex;
     .EasyTable {
       display: flex;
       flex-wrap: wrap;
+      max-width: 100%;
       justify-content: center;
-      width: 100%;
-      margin: 30px;
-      -webkit-box-shadow: 0px 4px 8px 0px rgba(34, 60, 80, 0.2);
-      -moz-box-shadow: 0px 4px 8px 0px rgba(34, 60, 80, 0.2);
-      box-shadow: 0px 4px 8px 0px rgba(34, 60, 80, 0.2);
+      margin-left: 30px;
+      margin-right: 30px;
+      -webkit-box-shadow: 0 4px 8px 0 rgba(34, 60, 80, 0.2);
+      -moz-box-shadow: 0 4px 8px 0 rgba(34, 60, 80, 0.2);
+      box-shadow: 0 4px 8px 0 rgba(34, 60, 80, 0.2);
+
       &-header {
         display: block;
         width: 100%;
         margin-top: 20px;
+
         tr {
+          display: -webkit-flex;
           display: flex;
-          justify-content: space-around;
           width: 100%;
           padding: 10px 0;
           border-bottom: 1px solid rgba(34, 60, 80, 0.7);
+
           th {
-            flex: 1;
+            width: 300px;
+            flex-basis: 25%;
             text-align: center;
+
+            &:-ms-fullscreen, :root .IE-FlexAuto {
+              flex-basis: auto;
+            }
           }
         }
       }
+
       &-body {
         display: block;
         width: 100%;
-        tr {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          margin-top: 10px;
-          -webkit-box-shadow: 0px 4px 8px 0px rgba(34, 60, 80, 0.2);
-          -moz-box-shadow: 0px 4px 8px 0px rgba(34, 60, 80, 0.2);
-          box-shadow: 0px 4px 8px 0px rgba(34, 60, 80, 0.2);
-        }
       }
     }
-  }
 </style>
